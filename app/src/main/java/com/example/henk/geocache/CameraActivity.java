aa55fde0ab191a6f3975090b7fe055cfd49d6806
Paolo <pcaraos@uci.edu>
2016-05-29 23:06:23 -0700

    Updated CameraActivity

--- app/src/main/java/com/example/henk/geocache/CameraActivity.java
+++ app/src/main/java/com/example/henk/geocache/CameraActivity.java
@@ -1,14 +1,75 @@
 package com.example.henk.geocache;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 public class CameraActivity extends AppCompatActivity {
+
+    /* for the camera*/
+    CameraPreview cv;
+    Filter filter;
+    FrameLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+
+        //setContentView(R.layout.activity_camera);
+
+        /* for camera*/
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    public void Load(){
+        Camera camera = getCamera();
+        if(camera != null){
+            layout = new FrameLayout(this);
+            layout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+
+            cv = new CameraPreview(this,camera);
+            layout.addView(cv);
+            filter = new Filter(this);
+            layout.addView(filter);
+
+            setContentView(layout);
+        }
+        else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Unable to find camera. Closing", Toast.LENGTH_SHORT);
+            toast.show();
+            finish();
+        }
+    }
+
+    public static Camera getCamera(){
+        Camera camera = null;
+        try{
+            camera = Camera.open();
+        }
+        catch(Exception e){
+        }
+        return camera;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(cv != null){
+            cv.onPause();
+            cv = null;
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Load();
+    }
+
 }
--- app/src/main/java/com/example/henk/geocache/MapsActivity.java
+++ app/src/main/java/com/example/henk/geocache/MapsActivity.java
@@ -1,14 +1,9 @@
 package com.example.henk.geocache;
 
-import android.content.pm.ActivityInfo;
-import android.hardware.Camera;
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,76 +13,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    /* for the camera*/
-    CameraPreview cv;
-    Filter filter;
-    FrameLayout layout;
+
 
     private GoogleMap mMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
-        /* for camera*/
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        setUpMapIfNeeded();
     }
-
-    public void Load(){
-        Camera camera = getCamera();
-        if(camera != null){
-            layout = new FrameLayout(this);
-            layout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
-
-            cv = new CameraPreview(this,camera);
-            layout.addView(cv);
-            filter = new Filter(this);
-            layout.addView(filter);
-
-            setContentView(layout);
-        }
-        else {
-            Toast toast = Toast.makeText(getApplicationContext(), "Unable to find camera. Closing", Toast.LENGTH_SHORT);
-            toast.show();
-            finish();
-        }
-    }
-
-    public static Camera getCamera(){
-        Camera camera = null;
-        try{
-            camera = Camera.open();
-        }
-        catch(Exception e){
-        }
-        return camera;
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if(cv != null){
-            cv.onPause();
-            cv = null;
-        }
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Load();
-    }
-}
 
 
     /**
@@ -100,12 +37,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    public void onZoom(View view)
+    {
+        if(view.getId() == R.id.Bzoomin) {
+            mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        }
+        if(view.getId() == R.id.Bzoomout) {
+            mMap.animateCamera(CameraUpdateFactory.zoomOut());
+        }
+    }
+
+    public void onCamera(View view)
+    {
+       Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void setUpMapIfNeeded()
+    {
+        if(mMap == null) {
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+            if(mMap != null){
+                setUpMap();
+            }
+        }
+
+    }
+
+    private void setUpMap()
+    {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        try {
+            mMap.setMyLocationEnabled(true);
+        } catch (SecurityException se) {
+            System.out.println(se.getMessage());
+        }
     }
 }
